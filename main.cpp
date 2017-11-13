5ce5dc30fadd141664502c7e65365082e5865bc4
Luis Moreno <luismoreno@Luiss-MacBook-Pro.local>
2017-11-12 21:08:16 -0600

    commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  team
+//
+//  Created by Luis Moreno on 11/12/17.
+//  Copyright © 2017 luismoreno. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
